3711c5abf6a4ebe18d9709578087e26b4aa3d9a1
AzureAD\MayurChawla <mayur.chawla@active.ai>
2021-08-17 18:27:43 +0530

    basic files

---
+++ background2.js
@@ -0,0 +1,13 @@
+// chrome.browserAction.onClicked.addListener(function(){
+//   chrome.tabs.create({'url':"abcd"})
+// });
+
+
+// // background.js
+
+// let color = '#3aa757';
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
